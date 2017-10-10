b94057a5567abe5250f0c06d0ef5761a02a3f8f4
Maria Fedorova <polosatik@yandex-team.ru>
2017-10-10 11:48:15 +0300

    sleeping thread and daemon

--- src/five/presentation/Presentation.java
+++ src/five/presentation/Presentation.java
@@ -4,12 +4,28 @@ package five.presentation;
  * Created by polosatik on 10.10.17.
  */
 public class Presentation {
-    public static void main(String[] args) {
-        System.out.println(Thread.currentThread().getName());
-        MyThread newThread = new MyThread();
+    public static void main(String[] args) throws InterruptedException {
+//        System.out.println(Thread.currentThread().getName());
+//        MyThread newThread = new MyThread();
+//
+//        newThread.start();
+//        System.out.println(Thread.currentThread().getName());
 
-        newThread.start();
         System.out.println(Thread.currentThread().getName());
+
+        Thread newThread = new Thread(() -> {
+            try {
+                System.out.println("Sleeping...");
+                Thread.currentThread().sleep(1000L);
+                System.out.println(Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } );
+
+        newThread.setDaemon(true);
+        newThread.start();
+
 
 
 
