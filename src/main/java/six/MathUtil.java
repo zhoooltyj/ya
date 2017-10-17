fce20bb347d0e43cffd5eb450975cd85e2cd4a45
Maria Fedorova <polosatik@yandex-team.ru>
2017-10-17 18:17:30 +0300

    easy operations for testing

---
+++ src/main/java/six/MathUtil.java
@@ -0,0 +1,15 @@
+package six;
+
+/**
+ * Created by polosatik on 17.10.17.
+ */
+public class MathUtil {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
